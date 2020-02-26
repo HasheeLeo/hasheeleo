057e5626dad56e1fb96db846825620d810ca7d6c
Hashir Ahmad <hashir@hashirahmad.com>
2020-02-26 15:24:00 +0500

    Add big portfolio images

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -1,35 +1,41 @@
-const Portfolio = () => {
-  const kitchat = [
-    {
-      original: '/kitchat-1.png',
-      thumbnail: '/kitchat-1.png'
-    },
-
-    {
-      original: '/kitchat-1.png',
-      thumbnail: '/kitchat-1.png'
-    }
-  ];
-
-  return (
-    <section>
-      <p className='cool-text'>My React Native Apps</p>
-      <article>
-        <header><p className='cool-subtext'>KitChat - A Chat App</p></header>
+const Portfolio = () => (
+  <section>
+    <p className='cool-text'>My React Native Apps</p>
+    <article>
+      <header><p className='cool-subtext'>KitChat - A Chat App</p></header>
+      <a href='/kitchat-1-bg.png' target='_blank'>
         <img src='/kitchat-1.png' alt='One on One Chatting Screen' />
+      </a>
+      <a href='/kitchat-2-bg.png' target='_blank'>
         <img src='/kitchat-2.png' alt='Message Threads Screen' />
+      </a>
+      <a href='/kitchat-3-bg.png' target='_blank'>
         <img src='/kitchat-3.png' alt='Sign Up Screen' />
-      </article>
+      </a>
+    </article>
 
-      <article>
-        <header><p className='cool-subtext'>Hangout Time - Schedule Meetups when Everybody's Free</p></header>
+    <article>
+      <header><p className='cool-subtext'>Hangout Time - Schedule Meetups when Everybody's Free</p></header>
+      <a href='/hangouttime-1-bg.png' target='_blank'>
         <img src='/hangouttime-1.png' alt='Create a New Event Screen' />
+      </a>
+      <a href='/hangouttime-2-bg.png' target='_blank'>
         <img src='/hangouttime-2.png' alt='Choose Dates and Times for Event Screen' />
+      </a>
+      <a href='/hangouttime-3-bg.png' target='_blank'>
         <img src='/hangouttime-3.png' alt='Events you are Going to Screen' />
+      </a>
+      <a href='/hangouttime-4-bg.png' target='_blank'>
         <img src='/hangouttime-4.png' alt='App Drawer' />
-      </article>
-    </section>
-  );
-};
+      </a>
+    </article>
+
+    <style jsx>{`
+      a {
+        border-bottom: none;
+      }
+    `}</style>
+  </section>
+);
 
 export default Portfolio;
